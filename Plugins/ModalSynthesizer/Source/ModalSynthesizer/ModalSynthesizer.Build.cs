2d9216646f1459fc7840d9a48793a7d75c89ce7e
asawinSit <asawin.sitthi@outlook.com>
2026-03-05 02:10:08 +0100

    add custom node

---
+++ Plugins/ModalSynthesizer/Source/ModalSynthesizer/ModalSynthesizer.Build.cs
@@ -0,0 +1,59 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ModalSynthesizer : ModuleRules
+{
+	public ModalSynthesizer(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+				"MetasoundFrontend"
+,
+				"MetasoundGraphCore"
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...
+				"MetasoundEngine",	
+				"MetasoundFrontend",  // <--- This provides TInputDataVertexModel
+				"MetasoundGraphCore", // <--- This provides the Operator logic
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Plugins/ModalSynthesizer/Source/ModalSynthesizer/Public/NewFile1.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Volume.h"
+#include "MetasoundEnumRegistrationMacro.h"
+#include "MetasoundParamHelper.h"
+
+namespace DSPTemplate
+{
+	class FVolumeOperator : public Metasound::TExecutableOperator<FVolumeOperator>
+	{
+	public:
+		FVolumeOperator(const Metasound::FOperatorSettings& InSettings, const Metasound::FAudioBufferReadRef& InAudioInput, const Metasound::FFloatReadRef& InAmplitude);
+
+		static const Metasound::FNodeClassMetadata& GetNodeInfo();
+
+		virtual void BindInputs(Metasound::FInputVertexInterfaceData& InOutVertexData) override;
+		virtual void BindOutputs(Metasound::FOutputVertexInterfaceData& InOutVertexData) override;
+
+		static const Metasound::FVertexInterface& GetVertexInterface();
+		static TUniquePtr<Metasound::IOperator> CreateOperator(const Metasound::FBuildOperatorParams& InParams, Metasound::FBuildResults& OutResults);
+
+		void Execute();
+
+		void Reset(const IOperator::FResetParams& InParams);
+
+	private:
+		Metasound::FAudioBufferReadRef	AudioInput;
+		Metasound::FAudioBufferWriteRef AudioOutput;
+
+		DSPProcessing::FVolume VolumeDSPProcessor;
+
+		Metasound::FFloatReadRef Amplitude;
+	};
+
+	using FVolumeNode = Metasound::TNodeFacade<FVolumeOperator>;
+}
